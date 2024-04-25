92f5593df1d5a2133500dea51e212ec04098b91a
Sheer Jay A. Piodos <82576185+jogzfon@users.noreply.github.com>
2024-04-25 10:06:09 +0800

    Add files via upload
    wew

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,49 +9,47 @@ from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
 import pandas as pd
-ps=PorterStemmer()   
+
+ps = PorterStemmer()
 from xgboost import XGBClassifier
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
 
-
 def transform_text(text):
-        text=text.lower()
-        y=[]
-        #tokenization
-        text=nltk.word_tokenize(text)
-        for i in text:
-            if i.isalnum():
-                y.append(i)
-        text=y[:]
-        y.clear()
-        #removing stopwords and punctuations
-        for i in text:
-            if i not in stopwords.words('english') and i not in string.punctuation:
-                y.append(i)
-        text=y[:]
-        y.clear()
-    
-        #stemming applied on text
-        for i in text:
-            y.append(ps.stem(i))
-        return y
+    text = text.lower()
+    y = []
+    # tokenization
+    text = nltk.word_tokenize(text)
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    # removing stopwords and punctuations
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    text = y[:]
+    y.clear()
+
+    # stemming applied on text
+    for i in text:
+        y.append(ps.stem(i))
+    return y
 
 
+# remove SMS_Spam_Classifier name from path while deploying locally
 
-
-#remove SMS_Spam_Classifier name from path while deploying locally 
-
-tfidf=pickle.load(open('SMS_Spam_Classifier/vectorizer.pkl','rb'))
-model=pickle.load(open('SMS_Spam_Classifier/mnb_spam_detector.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('mnb_spam_detector.pkl', 'rb'))
 
 st.title("SMS Spam classifier")
 
-#content
+# content
 
-st.image(Image.open('SMS_Spam_Classifier/spam_image.jpeg'))
+st.image(Image.open('spam_image.jpeg'))
 
 st.write("""
 A spam classifier uses machine learning to distinguish between legitimate and unsolicited emails . it employs algorithm to analyze content and other features to flag emails spam or not spam.
@@ -59,44 +57,38 @@ A spam classifier uses machine learning to distinguish between legitimate and un
 Algorithm used to train the model is stacking classifier(SVM,NB,Xgboost)
 
 """
- 
-)
 
+         )
 
-
-
-input_sms= st.text_area("Enter the message to check")
-
+input_sms = st.text_area("Enter the message to check")
 
 if st.button('Predict'):
 
-    #1.preprocess    
-    transform_sms=transform_text(input_sms)
+    # 1.preprocess
+    transform_sms = transform_text(input_sms)
     print(type(transform_sms))
-    transform_sms=np.array(transform_sms)
-    #2.vectorize
-    vector_input=tfidf.transform(transform_sms.astype('str')).toarray()
+    transform_sms = np.array(transform_sms)
+    # 2.vectorize
+    vector_input = tfidf.transform(transform_sms.astype('str')).toarray()
     print(type(vector_input))
     print(vector_input)
-    vector_input = pd.DataFrame(vector_input,columns=tfidf.get_feature_names_out())
+    vector_input = pd.DataFrame(vector_input, columns=tfidf.get_feature_names_out())
 
-    #3.predict
+    # 3.predict
 
-    prediction= model.predict(vector_input)[0]
-    #4.display
-    #st.header("Spam") if prediction else st.header("Not Spam")
-    if prediction==1:
+    prediction = model.predict(vector_input)[0]
+    # 4.display
+    # st.header("Spam") if prediction else st.header("Not Spam")
+    if prediction == 1:
         st.header("Spam")
     else:
         st.header("Not Spam")
-        
 
-c1,c2,c3 = st.columns(3)
+c1, c2, c3 = st.columns(3)
 with c1:
-    st.info('**GitHub:[@anilremo23](https://github.com/anilremo23)**',icon="🧠")
+    st.info('**GitHub:[@jogzfon](https://github.com/jogzfon)**', icon="🧠")
 with c2:
-    st.info('**Kaggle:[@remoanil](https://www.kaggle.com/remoanil)**',icon="💻")
+    st.info('**Kaggle:[@Sheer Jay Piodos](https://www.kaggle.com/shrivernrennassies)**', icon="💻")
 with c3:
-    st.info('**LinkedIn:[@AnilMamidwar](https://www.linkedin.com/in/anil-mamidwar-001b6418/)**',icon="👨‍💼")
-    
-      
+    st.info('**LinkedIn:[@Sheer Jay Piodos](https://www.linkedin.com/in/sheer-jay-piodos-87711329a/)**', icon="👨‍💼")
+
